2eba856c1a437ac67d79e457c2a8f27bd3446728
kate20031 <83357098+kate20031@users.noreply.github.com>
2026-06-04 16:11:40 +0000

    Add token type definitions

--- individual_assignment/main.cpp
+++ individual_assignment/main.cpp
@@ -2,6 +2,21 @@
 
 using namespace std;
 
+enum class TokenType {
+    INT,
+    FLOAT,
+    STRING,
+    COMMENT,
+    IDENTIFIER,
+    KEYWORD,
+     SYMBOL,
+    UNKNOWN,
+    ERROR_STRING,
+    ERROR_COMMENT,
+    ERROR_FLOAT,
+    END_OF_FILE
+};
+
 int main() {
     cout << "OCaml scanner started" << endl;
     return 0;
