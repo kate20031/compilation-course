5622e7ddc3f0f656ea485d4b24287c10a24bb503
kate20031 <83357098+kate20031@users.noreply.github.com>
2026-06-05 17:09:27 +0000

    Add token type string conversion

--- individual_assignment/main.cpp
+++ individual_assignment/main.cpp
@@ -25,6 +25,24 @@ struct Token {
     int column;
 };
 
+string tokenTypeToString(TokenType type) {
+    switch (type) {
+        case TokenType::INT: return "INT";
+        case TokenType::FLOAT: return  "FLOAT";
+        case TokenType::STRING: return  "STRING";
+        case TokenType::COMMENT: return "COMMENT";
+        case TokenType::IDENTIFIER:  return "IDENTIFIER";
+        case TokenType::KEYWORD: return "KEYWORD";
+        case TokenType::SYMBOL: return "SYMBOL";
+        case  TokenType::UNKNOWN: return "UNKNOWN";
+        case TokenType::ERROR_STRING: return "ERROR_STRING";
+        case TokenType::ERROR_COMMENT: return "ERROR_COMMENT";
+        case TokenType::ERROR_FLOAT: return "ERROR_FLOAT";
+        case TokenType::END_OF_FILE:  return "END_OF_FILE";
+        default: return "UNDEFINED";
+    }
+}
+
 int main() {
     cout << "OCaml scanner started" << endl;
     return 0;
